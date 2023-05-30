2a40f45969556414fd8b36774ebd7058dc84a987
Vladislav Tolkachev <tvladick1996@yandex.ru>
2023-05-30 23:16:03 +0300

    finish day2 - правка версти авторизации + подгонка под бэм

--- src/pages/Authorization.vue
+++ src/pages/Authorization.vue
@@ -4,14 +4,18 @@
 
 <template>
     <div class="authorization">
-        <div class="authorization__content">
-            <div class="authorization__header">Вход в личный кабинет</div>
-            <div class="authorization__info">Введите данные для входа в личный кабинет администратора</div>
-            <div class="authorization__login">
-
+        <div class="authorization-content">
+            <div class="authorization-header">Вход в личный кабинет</div>
+            <div class="authorization-info">Введите данные для входа в личный кабинет администратора</div>
+            <div class="authorization-login">
+                <div class="authorization-login-content">
+                    <span>Введите номер телефона</span>
+                    <input class="authorization-login-content__input" type="text">
+                </div>
             </div>
-            <div class="authorization__pass">parol</div>
-            <button class="authorization__button">Войти в кабинет</button>
+            <input type="password" class="authorization-pass" placeholder="Пароль">
+            <!--<div class="authorization-pass">parol</div>-->
+            <button class="authorization-button">Войти в кабинет</button>
         </div>
     </div>
 </template>
@@ -24,7 +28,7 @@
     border: 1px solid #126CF3;
 }
 
-.authorization__content {
+.authorization-content {
     margin: 31.5px;
     display: flex;
     flex-direction: column;
@@ -32,7 +36,7 @@
 
 }
 
-.authorization__header {
+.authorization-header {
     width: 100%;
     height: 40px;
     margin-bottom: 10px;
@@ -43,7 +47,7 @@
     color: #0E0E0E;
 }
 
-.authorization__info {
+.authorization-info {
     width: 70%;
     height: 50px;
     font-weight: 400;
@@ -54,23 +58,52 @@
     margin-bottom: 25px;
 }
 
-.authorization__login {
+.authorization-login {
     width: 100%;
     height: 50px;
     border: 0.75px solid #9E9E9E;
     border-radius: 3px;
     margin-bottom: 15px;
+
 }
 
-.authorization__pass {
+.authorization-login-content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    margin: 7.5px 18px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+    color: #585858;
+}
+
+.authorization-login-content__input {
+    width: 100%;
+    font-size: 13.4981px;
+    line-height: 18px;
+}
+
+.authorization-pass {
     width: 100%;
     height: 50px;
-    background: #F1F5F9;
     border-radius: 3px;
     margin-bottom: 15px;
+    background: #F1F5F9;
+    border: none;
 }
 
-.authorization__button {
+.authorization-pass::-webkit-input-placeholder {
+    padding: 10px;
+}
+
+.authorization-pass::placeholder {
+    padding: 15px 20px;
+}
+
+.authorization-button {
     width: 100%;
     height: 55px;
     background: #126CF3;
