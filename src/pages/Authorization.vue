1d4892daa919c685dd8b75e8010afcd1c4490f24
Vladislav Tolkachev <tvladick1996@yandex.ru>
2023-05-30 22:42:40 +0300

    start day2

--- src/pages/Authorization.vue
+++ src/pages/Authorization.vue
@@ -4,12 +4,14 @@
 
 <template>
     <div class="authorization">
-        <div class="authorization-content">
-            <div class="authorization-header">Вход в личный кабинет</div>
-            <div class="authorization-info">Введите данные для входа в личный кабинет администратора</div>
-            <div class="authorization-login">телефон</div>
-            <div class="authorization-pass">parol</div>
-            <button class="authorization-button">Войти в кабинет</button>
+        <div class="authorization__content">
+            <div class="authorization__header">Вход в личный кабинет</div>
+            <div class="authorization__info">Введите данные для входа в личный кабинет администратора</div>
+            <div class="authorization__login">
+
+            </div>
+            <div class="authorization__pass">parol</div>
+            <button class="authorization__button">Войти в кабинет</button>
         </div>
     </div>
 </template>
@@ -19,11 +21,10 @@
     height: 365px;
     width: 500px;
     border-radius: 6px;
-
     border: 1px solid #126CF3;
 }
 
-.authorization-content {
+.authorization__content {
     margin: 31.5px;
     display: flex;
     flex-direction: column;
@@ -31,12 +32,10 @@
 
 }
 
-.authorization-header {
+.authorization__header {
     width: 100%;
     height: 40px;
     margin-bottom: 10px;
-    font-family: 'Manrope';
-    font-style: normal;
     font-weight: 700;
     font-size: 31.4955px;
     line-height: 39px;
@@ -44,11 +43,9 @@
     color: #0E0E0E;
 }
 
-.authorization-info {
+.authorization__info {
     width: 70%;
     height: 50px;
-    font-family: 'Manrope';
-    font-style: normal;
     font-weight: 400;
     font-size: 14.9979px;
     line-height: 24px;
@@ -57,7 +54,7 @@
     margin-bottom: 25px;
 }
 
-.authorization-login {
+.authorization__login {
     width: 100%;
     height: 50px;
     border: 0.75px solid #9E9E9E;
@@ -65,7 +62,7 @@
     margin-bottom: 15px;
 }
 
-.authorization-pass {
+.authorization__pass {
     width: 100%;
     height: 50px;
     background: #F1F5F9;
@@ -73,7 +70,7 @@
     margin-bottom: 15px;
 }
 
-.authorization-button {
+.authorization__button {
     width: 100%;
     height: 55px;
     background: #126CF3;
